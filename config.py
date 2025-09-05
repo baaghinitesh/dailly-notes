0cc70219e89cf7e09d53c2da4c7e4a10f42492b0
Nitesh jha <137025928+baaghinitesh@users.noreply.github.com>
2025-09-05 13:12:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,1 @@
-# API_KEY = "AIzaSyBxODkEMsG6Pi3MTYWJjhw17P2L9CT3plU"
-GITHUB_REPO = "https://github.com/baaghinitesh/dailly-notes.git"  # Replace with your repo URL
-AUTHOR_NAME = "baaghinitesh"
-AUTHOR_EMAIL = "baaghinitesh@gmail.com"
+
