0047efdcca3dfce539beda99b733c0d9f5dc653e
baaghinitesh <baaghinitesh@gmail.com>
2025-09-05 17:30:42 +0000

    📘 Added DSA solution: Sort Colors

---
+++ docs/dsa/medium/SortColors.java
@@ -0,0 +1,33 @@
+// Sort Colors
+// Difficulty: Medium
+class SortColors {
+    public void sortColors(int[] nums) {
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+
+        for (int num : nums) {
+            if (num == 0) {
+                zero++;
+            } else if (num == 1) {
+                one++;
+            } else {
+                two++;
+            }
+        }
+
+        int index = 0;
+        while (zero > 0) {
+            nums[index++] = 0;
+            zero--;
+        }
+        while (one > 0) {
+            nums[index++] = 1;
+            one--;
+        }
+        while (two > 0) {
+            nums[index++] = 2;
+            two--;
+        }
+    }
+}
