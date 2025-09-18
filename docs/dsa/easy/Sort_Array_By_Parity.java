d73db2abbb0d874c26e5669c4f1ab75eaadf8e09
baaghinitesh <baaghinitesh@gmail.com>
2025-09-18 01:31:16 +0000

    📘 Added DSA solution: Sort Array By Parity

---
+++ docs/dsa/easy/Sort_Array_By_Parity.java
@@ -0,0 +1,18 @@
+//Sort Array By Parity
+//Easy
+class SortArrayByParity {
+    public int[] sortArrayByParity(int[] nums) {
+        int[] result = new int[nums.length];
+        int evenIndex = 0;
+        int oddIndex = nums.length - 1;
+
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                result[evenIndex++] = num;
+            } else {
+                result[oddIndex--] = num;
+            }
+        }
+        return result;
+    }
+}
